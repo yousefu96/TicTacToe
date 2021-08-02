2f40e1e3a56d176192ca9caa104ee525cdc4d961
yousefu96 <77845605+yousefu96@users.noreply.github.com>
2021-08-02 17:19:32 -0400

    Add files via upload

--- TicTacToe.c
+++ TicTacToe.c
@@ -4,217 +4,213 @@
 /*
 Author: Usman Yousef
 Date: 07/27/2021
-Description: This is my first, primitive version of Tic-Tac-Toe
+Description: This is my first, primitive version of TicTacToe
+Corrected function signatures, replaced 2D array with a 1D array, and corrected logic error in OTurn()
+Looking to make final correction to proper displaying of the grid which seems "lagged" in output...
 */
 
-void Check4Winner();
-void XTurn();
-void OTurn();
-void InitializeGrid();
-void DisplayGrid();
-void TestFunc();
+int Check4Winner(int* pArray);
+void XTurn(int* pArray);
+void OTurn(int* pArray);
+void InitializeGrid(int* pArray);
+void DisplayGrid(int* pArray);
+void DisplayResult(int win, int turnCount);
 
 int main()
 {
     //declarations
-    int array [3][3];
-    int turnCount = 1;
+    int array [9];
+    int turnCount = 0;
+    int win = 0;
 
-    //printf("%d \n", array);
-    //printf("%d \n", *array);
     InitializeGrid(array);
-    DisplayGrid(array);
 
-    while(turnCount < 10)
+    while(win == 0 && turnCount < 9)
     {
+        DisplayGrid(array);
         XTurn(array);
-        turnCount++;
-        DisplayGrid(array);
-        Check4Winner(&turnCount, array);
+        ++turnCount;
+        win = Check4Winner(array);
 
-        OTurn(array);
-        turnCount++;
+        if(win == 1)
+        {
+            break;
+        }
+
         DisplayGrid(array);
-        Check4Winner(&turnCount, array);
+        OTurn(array);
+        ++turnCount;
+        win = Check4Winner(array);
+    }
+
+    DisplayGrid(array);
+    DisplayResult(win, turnCount);
+    return 0;
+}
+//================================================================================================================================================================================================================
+void XTurn (int* pArray)
+{
+    int num = 0;
+
+    while(num < 2 || num > 10)
+    {
+        printf("\nPlease select a valid number...\n");
+        scanf("%d \n", &num);
+    }
+
+    for(int x = 0; x < 9; x++)
+    {
+        if (num == pArray[x])
+        {
+            pArray[x] = 1;
+        }
+    }
+}
+//================================================================================================================================================================================================================
+void OTurn (int* pArray)
+{
+    int num = 0;
+
+    while(num < 2 || num > 10)
+    {
+        printf("\nPlease select a valid number...\n");
+        scanf("%d \n", &num);
+    }
+    for(int x = 0; x < 9; x++)
+    {
+        if (num == pArray[x])
+        {
+            pArray[x] = 0;
+        }
+    }
+}
+//================================================================================================================================================================================================================
+int Check4Winner(int* pArray)
+{
+    //O's winning conditions
+    if(pArray[0] && pArray[1] && pArray[2] == 0)
+    {
+        //printf("O WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[3] && pArray[4] && pArray[5] == 0)
+    {
+        //printf("O WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[6] && pArray[7] && pArray[8] == 0)
+    {
+        //printf("O WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[0] && pArray[3] && pArray[6] == 0)
+    {
+        //printf("O WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[1] && pArray[4] && pArray[7] == 0)
+    {
+        //printf("O WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[2] && pArray[5] && pArray[8] == 0)
+    {
+        //printf("O WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[0] && pArray[4] && pArray[8] == 0)
+    {
+        //printf("O WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[6] && pArray[4] && pArray[2] == 0)
+    {
+        //printf("O WINS!!!\n");
+        return 1;
+    }
+
+    //X's winning conditions
+    if(pArray[0] && pArray[1] && pArray[2] == 1)
+    {
+        //printf("X WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[3] && pArray[4] && pArray[5] == 1)
+    {
+        //printf("X WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[6] && pArray[7] && pArray[8] == 1)
+    {
+        //printf("X WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[0] && pArray[3] && pArray[6] == 1)
+    {
+        //printf("X WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[1] && pArray[4] && pArray[7] == 1)
+    {
+        //printf("X WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[2] && pArray[5] && pArray[8] == 1)
+    {
+        //printf("X WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[0] && pArray[4] && pArray[8] == 1)
+    {
+        //printf("X WINS!!!\n");
+        return 1;
+    }
+    else if(pArray[6] && pArray[4] && pArray[2] == 1)
+    {
+        //printf("X WINS!!!\n");
+        return 1;
     }
     return 0;
 }
 //================================================================================================================================================================================================================
-void XTurn (int** pArray[])
+void DisplayGrid(int* pArray)
 {
-    int num = 0;
-
-    while(num < 2 || num > 10)
+    int lineLimit = 0;
+    for(int x = 0; x < 9; x++)
     {
-        printf("\nPlease select a valid number...\n");
-        scanf("%d \n", &num);
-    }
-    for(int x = 0; x < 3; x++)
-    {
-        for(int y = 0; y < 3; y++)
+        printf("%d  ", pArray[x]);
+        lineLimit++;
+        if(lineLimit == 3)
         {
-            if (num == pArray[x][y])
-            {
-                pArray[x][y] = 1;
-            }
+            printf("\n");
+            lineLimit = 0;
         }
     }
 }
 //================================================================================================================================================================================================================
-void OTurn (int** pArray[])
-{
-    int num = 0;
-
-    while(num < 2 || num > 10)
-    {
-        printf("\nPlease select a valid number...\n");
-        scanf("%d \n", &num);
-    }
-    for(int x = 0; x < 3; x++)
-    {
-        for(int y = 0; y < 3; y++)
-        {
-            if (num == pArray[x][y])
-            {
-                pArray[x][y] = 0;
-            }
-        }
-    }
-}
-//================================================================================================================================================================================================================
-void Check4Winner(int turnCount, int** pArray[])
-{
-    int checkWin = 0;
-    //O's winning conditions
-    if(pArray[0][0] && pArray[0][1] && pArray[0][2] == 0)
-    {
-        printf("O WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[0][0] && pArray[1][0] && pArray[2][0] == 0)
-    {
-        printf("O WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[2][0] && pArray[2][1] && pArray[2][2] == 0)
-    {
-        printf("O WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[0][1] && pArray[1][1] && pArray[2][1] == 0)
-    {
-        printf("O WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[0][1] && pArray[1][1] && pArray[2][1] == 0)
-    {
-        printf("O WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[0][2] && pArray[1][2] && pArray[2][2] == 0)
-    {
-        printf("O WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[2][0] && pArray[1][1] && pArray[0][2] == 0)
-    {
-        printf("O WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[0][0] && pArray[1][1] && pArray[2][2] == 0)
-    {
-        printf("O WINS!!!");
-        checkWin++;
-        return 0;
-    }
-
-    //X's winning conditions
-    if(pArray[0][0] && pArray[0][1] && pArray[0][2] == 1)
-    {
-        printf("X WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[0][0] && pArray[1][0] && pArray[2][0] == 1)
-    {
-        printf("X WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[2][0] && pArray[2][1] && pArray[2][2] == 1)
-    {
-        printf("X WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[0][1] && pArray[1][1] && pArray[2][1] == 1)
-    {
-        printf("X WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[0][1] && pArray[1][1] && pArray[2][1] == 1)
-    {
-        printf("X WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[0][2] && pArray[1][2] && pArray[2][2] == 1)
-    {
-        printf("X WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[2][0] && pArray[1][1] && pArray[0][2] == 1)
-    {
-        printf("X WINS!!!");
-        checkWin++;
-        return 0;
-    }
-    else if(pArray[0][0] && pArray[1][1] && pArray[2][2] == 1)
-    {
-        printf("X WINS!!!");
-        checkWin++;
-        return 0;
-    }
-
-    if (checkWin == 0 && turnCount == 9)
-    {
-        printf("DRAW!!!");
-        return 0;
-    }
-}
-//================================================================================================================================================================================================================
-void DisplayGrid(int** pArray[])
-{
-    for(int x = 0; x < 3; x++)
-    {
-        printf("\n");
-        for(int y = 0; y < 3; y++)
-        {
-            printf("%d  ", pArray[x][y]);
-        }
-    }
-}
-//================================================================================================================================================================================================================
-void InitializeGrid(int** pArray[])
+void InitializeGrid(int* pArray)
 {
     int i = 2;
 
-    for(int x = 0; x < 3; x++)
+    for(int x = 0; x < 9; x++)
     {
-        for(int y = 0; y < 3; y++)
-        {
-            pArray[x][y] = i;
-            i++;
-        }
+        pArray[x] = i;
+        i++;
     }
 }
 //================================================================================================================================================================================================================
+void DisplayResult(int win, int turnCount)
+{
+    if(turnCount % 2 == 0 && win == 1)
+    {
+        printf("O WINS!!!");
+    }
+    else if (turnCount % 2 != 0 && win == 1)
+    {
+        printf("X WINS!!!");
+    }
+    else
+    {
+        printf("Draw!");
+    }
+}
